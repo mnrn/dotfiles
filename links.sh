8b0c50721bf2c72ab51e539568037fc2ee9b60f4
ayase <paper.airplane01@gmail.com>
2018-04-09 01:02:37 +0900

    remove links.sh

--- links.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -sf ~/dotfiles/zsh/.zshrc      ~/.zshrc
-ln -sf ~/dotfiles/zsh/.zshenv     ~/.zshenv
-ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/vim/.vimrc      ~/.vimrc
-
-ln -sf ~/.vimrc ~/.config/nvim/init.vim
