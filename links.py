cf8deae4b1a1310c6a22e6f228b0cd4d1d01b074
ayase <paper.airplane01@gmail.com>
2018-05-07 23:46:38 +0900

    add logging.

--- links.py
+++ links.py
@@ -6,26 +6,29 @@
 
 import os
 import sys
+import logging
 
 
-# Home directory.
-HOME_DIR = os.environ['HOME']
+HOME_DIR = os.environ['HOME']                  # Home directory.
+DOT_DIR = os.path.join(HOME_DIR, '/dotfiles')  # Dotfiles directory.
 
-# Dotfiles directory.
-DOT_DIR = os.path.join(HOME_DIR, '/dotfiles')
+
+# Pre processing
+def entry():
+    logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s')
 
 
 def main():
     ignores = ['.git']  # ignore files
     rootdir = os.path.abspath(os.path.dirname(__file__))
-    for root, dirs, files in os.walk(rootdir):
+    for root, _, files in os.walk(rootdir):
         for file in files:
             if file[0] != '.' or any([file == ignore for ignore in ignores]):
                 continue
             linksrc = os.path.join(root, file)
             linkdst = os.path.join(HOME_DIR, file)
             os.symlink(linksrc, linkdst)
-            print("make symlink src={0}, dst={1}".format(linksrc, linkdst))
+            logging.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
 
 
 # Post processing
@@ -33,13 +36,15 @@ def exit():
     linksrc = os.path.join(HOME_DIR, '.vimrc')
     linkdst = os.path.join(HOME_DIR, '.config/nvim/init.vim')
     if not os.path.exists(linkdst):
+        logging.warn('not found file: '+ linkdst)
         return
 
     # make symlink for neovim.
     os.symlink(linksrc, linkdst)
-    print("make symlink src={0}, dst={1}".format(linksrc, linkdst))
+    logging.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
 
 
 if __name__ == '__main__':
+    entry()
     main()
     exit()
--- readme.md
+++ readme.md
@@ -1,22 +1,19 @@
 # dotfiles
 
-
-## Descpription
+## Overview
 
 I was exhausted from repeated environmental creation.<br>
 So I decided to upload dotfiles to github.<br>
 <br>
 There are descriptions about vim and zsh, tmux settings.
 
-
 ## Requirements
+
 - Git
 - Python 3.X
 
-<br>
-Please see Installation & Usage for more details.
+## Installation
 
-## Installation & Usage
 ```shell
 git clone https://github.com/mnrn/dotfiles
 cd dotfiles
