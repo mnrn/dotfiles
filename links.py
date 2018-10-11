5bfb498a19695d56411da92fd5e7521c3c032b1e
ayase <paper.airplane01@gmail.com>
2018-10-12 00:16:53 +0900

    Use logger.

--- links.py
+++ links.py
@@ -6,17 +6,19 @@
 
 import os
 import sys
-import logging as log
-
+import logging
 
 HOME_DIR = os.environ['HOME']                  # Home directory.
 DOT_DIR = os.path.join(HOME_DIR, '/dotfiles')  # Dotfiles directory.
 
+logger = logging.getLogger(__name__)
 
 # Pre processing
 def entry():
-    log.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s')
-
+    ch = logging.streamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+    ch.setFortmatter(formatter)
+    logger.addHandler(ch)
 
 def main():
     ignores = ['.git']  # ignore files
@@ -28,10 +30,10 @@ def main():
             linksrc = os.path.join(root, file)
             linkdst = os.path.join(HOME_DIR, file)
             if os.path.exists(linkdst):
-                log.warn('already exists: ' + linkdst)
+                logger.warn('already exists: ' + linkdst)
                 continue
             os.symlink(linksrc, linkdst)
-            log.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
+            logger.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
 
 
 # Post processing
@@ -39,12 +41,12 @@ def exit():
     linksrc = os.path.join(HOME_DIR, '.vimrc')
     linkdst = os.path.join(HOME_DIR, '.config/nvim/init.vim')
     if os.path.exists(linkdst):
-        log.warn('already exists: ' + linkdst)
+        logger.warn('already exists: ' + linkdst)
         return
 
     # make symlink for neovim.
     os.symlink(linksrc, linkdst)
-    log.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
+    logger.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
 
 
 if __name__ == '__main__':
