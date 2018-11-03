f8fb7d99a20b38a4ac14c8f3e7ed847a51818a06
ayase <paper.airplane01@gmail.com>
2018-11-03 18:20:01 +0900

    Create class Links and using it.

--- links.py
+++ links.py
@@ -8,48 +8,45 @@ import os
 import sys
 import logging
 
-HOME_DIR = os.environ['HOME']                  # Home directory.
-DOT_DIR = os.path.join(HOME_DIR, 'dotfiles')  # Dotfiles directory.
+class Links(object):
+    def __init__(self):
+        self.HOME_DIR = os.environ['HOME']                  # Home directory.
 
-logger = logging.getLogger(__name__)
-
-# Pre processing
-def entry():
-    ch = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-def main():
-    ignores = ['.git', '.gitignore', '.DS_Store']  # ignore files
-    rootdir = os.path.abspath(os.path.dirname(__file__))
-    for root, _, files in os.walk(rootdir):
-        for file in files:
-            if file[0] != '.' or any([file == ignore for ignore in ignores]):
-                continue
-            linksrc = os.path.join(root, file)
-            linkdst = os.path.join(HOME_DIR, file)
-            if os.path.exists(linkdst):
-                logger.warn('already exists: ' + linkdst)
-                continue
-            os.symlink(linksrc, linkdst)
-            logger.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
+        # Setting logger
+        self.logger = logging.getLogger(__name__)
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
 
-# Post processing
-def exit():
-    linksrc = os.path.join(HOME_DIR, '.vimrc')
-    linkdst = os.path.join(HOME_DIR, '.config/nvim/init.vim')
-    if os.path.exists(linkdst):
-        logger.warn('already exists: ' + linkdst)
-        return
+    def exec(self):
+        ignores = ['.git', '.gitignore', '.DS_Store']  # ignore files
+        rootdir = os.path.abspath(os.path.dirname(__file__))
+        for root, _, files in os.walk(rootdir):
+            for file in files:
+                if file[0] != '.' or any([file == ignore for ignore in ignores]):
+                    continue
+                linksrc = os.path.join(root, file)
+                linkdst = os.path.join(self.HOME_DIR, file)
+                if os.path.exists(linkdst):
+                    self.logger.warn('already exists: ' + linkdst)
+                    continue
+                os.symlink(linksrc, linkdst)
+                self.logger.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
 
-    # make symlink for neovim.
-    os.symlink(linksrc, linkdst)
-    logger.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
+    def __del__(self):
+        linksrc = os.path.join(self.HOME_DIR, '.vimrc')
+        linkdst = os.path.join(self.HOME_DIR, '.config/nvim/init.vim')
+        if os.path.exists(linkdst):
+            self.logger.warn('already exists: ' + linkdst)
+            return
+
+        # make symlink for neovim.
+        os.symlink(linksrc, linkdst)
+        self.logger.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
 
 
 if __name__ == '__main__':
-    entry()
-    main()
-    exit()
+    links = Links()
+    links.exec()
