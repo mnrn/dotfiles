03378f0b41b210617b6c54479f655844e439feb0
ayase <paper.airplane01@gmail.com>
2018-11-03 18:31:56 +0900

    Following to flake8.

--- links.py
+++ links.py
@@ -5,35 +5,40 @@
 
 
 import os
-import sys
 import logging
 
+
 class Links(object):
+
     def __init__(self):
-        self.HOME_DIR = os.environ['HOME']                  # Home directory.
+        self.HOME_DIR = os.environ['HOME']  # Home directory.
+        self.LOGGER_FORMAT = '%(asctime)s - %(levelname)s: %(message)s'
+        self.SYMLINKS_MSG = 'make symlink src={0}, dst={1}'
 
         # Setting logger
         self.logger = logging.getLogger(__name__)
         ch = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+        formatter = logging.Formatter(self.LOGGER_FORMAT)
         ch.setFormatter(formatter)
         self.logger.addHandler(ch)
-
 
     def exec(self):
         ignores = ['.git', '.gitignore', '.DS_Store']  # ignore files
         rootdir = os.path.abspath(os.path.dirname(__file__))
         for root, _, files in os.walk(rootdir):
             for file in files:
-                if file[0] != '.' or any([file == ignore for ignore in ignores]):
+                if file[0] != '.':
                     continue
+                elif any([file == ignore for ignore in ignores]):
+                    continue
+
                 linksrc = os.path.join(root, file)
                 linkdst = os.path.join(self.HOME_DIR, file)
                 if os.path.exists(linkdst):
                     self.logger.warn('already exists: ' + linkdst)
                     continue
                 os.symlink(linksrc, linkdst)
-                self.logger.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
+                self.logger.info(self.SYMLINKS_MSG.format(linksrc, linkdst))
 
     def __del__(self):
         linksrc = os.path.join(self.HOME_DIR, '.vimrc')
@@ -44,7 +49,7 @@ class Links(object):
 
         # make symlink for neovim.
         os.symlink(linksrc, linkdst)
-        self.logger.info('make symlink src={0}, dst={1}'.format(linksrc, linkdst))
+        self.logger.info(self.SYMLINKS_MSG.format(linksrc, linkdst))
 
 
 if __name__ == '__main__':
