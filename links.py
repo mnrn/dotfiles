a5dc1c643267d5f3cab3d58b9d4a40523563f8f6
ayase <paper.airplane01@gmail.com>
2018-11-04 11:57:00 +0900

    Rewrite around logger and link requirements.

--- links.py
+++ links.py
@@ -7,23 +7,26 @@
 import os
 import logging
 
-
 class Links(object):
 
-    def __init__(self):
+    def __init__(self, logger=None):
         self.HOME_DIR = os.environ['HOME']  # Home directory.
         self.LOGGER_FORMAT = '%(asctime)s - %(levelname)s: %(message)s'
         self.SYMLINKS_MSG = 'make symlink src={0}, dst={1}'
+        self.EXISTS_MSG = 'already exists: {0}'
 
         # Setting logger
-        self.logger = logging.getLogger(__name__)
-        ch = logging.StreamHandler()
-        formatter = logging.Formatter(self.LOGGER_FORMAT)
-        ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
+        if logger is None:
+            self.logger = logging.getLogger(__name__)
+            ch = logging.StreamHandler()
+            formatter = logging.Formatter(self.LOGGER_FORMAT)
+            ch.setFormatter(formatter)
+            self.logger.addHandler(ch)
+        else:
+            self.logger = logger
 
-    def exec(self):
-        ignores = ['.git', '.gitignore', '.DS_Store']  # ignore files
+
+    def exec(self, ignores):
         rootdir = os.path.abspath(os.path.dirname(__file__))
         for root, _, files in os.walk(rootdir):
             for file in files:
@@ -35,23 +38,27 @@ class Links(object):
                 linksrc = os.path.join(root, file)
                 linkdst = os.path.join(self.HOME_DIR, file)
                 if os.path.exists(linkdst):
-                    self.logger.warn('already exists: ' + linkdst)
+                    self.logger.warn(self.EXISTS_MSG.format(linkdst))
                     continue
                 os.symlink(linksrc, linkdst)
                 self.logger.info(self.SYMLINKS_MSG.format(linksrc, linkdst))
 
-    def __del__(self):
+
+    def neovim_symlink(self):
         linksrc = os.path.join(self.HOME_DIR, '.vimrc')
         linkdst = os.path.join(self.HOME_DIR, '.config/nvim/init.vim')
         if os.path.exists(linkdst):
-            self.logger.warn('already exists: ' + linkdst)
+            self.logger.warn(self.EXISTS_MSG.format(linkdst))
             return
 
-        # make symlink for neovim.
+        # Make symlink for neovim.
         os.symlink(linksrc, linkdst)
         self.logger.info(self.SYMLINKS_MSG.format(linksrc, linkdst))
 
 
 if __name__ == '__main__':
+
     links = Links()
-    links.exec()
+    links.exec(ignores=['.git', '.gitignore', '.DS_Store'])
+
+    links.neovim_symlink()
--- readme.md
+++ readme.md
@@ -9,21 +9,25 @@ There are descriptions about vim and zsh, tmux settings.
 
 ## Requirements
 
-- Git
-- Python 3.X
-- Zsh
-- Tmux
-- Oh-My-Zsh
-- Powerline
+- [Git](https://github.com/git/git)
+- [pyenv](https://github.com/pyenv/pyenv)
+- [Zsh](https://github.com/zsh-users/zsh)
+- [tmux](https://github.com/tmux/tmux)
+- [Oh-My-Zsh](https://github.com/robbyrussell/oh-my-zsh)
+- [Powerline](https://github.com/powerline/powerline)
 
 ### Options
 
-- neovim
-- peco
-- exa
-- ripgrep
-- ccat
-- colordiff
+- [Neovim](https://github.com/neovim/neovim)
+- [peco](https://github.com/peco/peco)
+- [exa](https://github.com/ogham/exa)
+- [ripgrep](https://github.com/BurntSushi/ripgrep)
+- [ccat](https://github.com/jingweno/ccat)
+- [colordiff](https://github.com/daveewart/colordiff)
+
+## Futures
+
+Enable Toml setting file.
 
 ## Installation
 
