d60cf65fdf6c484d705170fcf6fd69cb4c04c6ba
ayase <paper.airplane01@gmail.com>
2018-11-04 13:27:58 +0900

    Alias brew.

--- zsh/env/macosx/options.zsh
+++ zsh/env/macosx/options.zsh
@@ -1,10 +1,13 @@
 #-------------------------------------------------
 # options
 #-------------------------------------------------
-# add to path 
+# add to path
 export PATH=/usr/local/bin:$PATH
 export PATH=$PATH:$HOME/bin
 export PATH=$PATH:$HOME/include
 export PATH=$PATH:$HOME/local/bin
 export PATH=$PATH:$HOME/local/include
 export PATH=$PATH:$HOME/local/lib
+
+# Homebrew using zsh extension
+alias brew="env PATH=${${PATH/~\/\.local\/bin:/}/~\/\.pyenv\/shims:/} brew"
