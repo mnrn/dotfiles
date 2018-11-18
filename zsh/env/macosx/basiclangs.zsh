f00b6b64d6a224b0f99ecd28d6cf90995303c231
ayase <paper.airplane01@gmail.com>
2018-11-18 18:06:23 +0900

    Comment out CXXPATHS.

--- zsh/env/macosx/basiclangs.zsh
+++ zsh/env/macosx/basiclangs.zsh
@@ -3,16 +3,18 @@
 #-------------------------------------------------
 # clang
 export PATH=/usr/local/opt/llvm/bin:$PATH
-export LDFLAGS=-L/usr/local/opt/llvm/lib
-export CPPFLAGS=-I/usr/local/opt/llvm/include/c++/v1
+#export LDFLAGS=-L/usr/local/opt/llvm/lib
+#export CPPFLAGS=-I/usr/local/opt/llvm/include/c++/v1
+#export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include
+#export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATh:/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
 
 # gcc/clang
 export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$HOME/local/include
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/local/lib
 
 # cmake
-export CC=/usr/local/opt/llvm/bin/clang
-export CXX=/usr/local/opt/llvm/bin/clang++
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
 
 # pyenv
 export PYENV_ROOT=$HOME/.pyenv
