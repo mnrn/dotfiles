a505a943eda13c61421fb518e435897e638858f0
ayase <paper.airplane01@gmail.com>
2018-12-17 22:01:12 +0900

    Add android-studio path.

--- zsh/env/linux/apis.zsh
+++ zsh/env/linux/apis.zsh
@@ -1,6 +1,10 @@
 #-------------------------------------------------
 # api
 #-------------------------------------------------
+
+# android
+export PATH=$PATH:$HONE/local/android-studio/bin
+
 # vulkan
 export VULKAN_SDK=$HOME/local/VulkanSDK/1.0.57.0/x86_64
 export PATH=$PATH:$VULKAN_SDK/bin
