08196607c14a3e1274a21c3be57e2c7dae34e2ac
ayase <paper.airplane01@gmail.com>
2019-01-27 16:00:54 +0900

    Read powerline files only when setting file exists.

--- zsh/rc/powerline.zsh
+++ zsh/rc/powerline.zsh
@@ -1,3 +1,5 @@
 # powerline
-powerline-daemon -q
-. ~/.local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
+if [[ -f ~/.local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
+    powerline-daemon -q
+    . ~/.local/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
+fi
